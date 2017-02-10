ce661f53e3a548c95dad43ef421926ce9719d3fe
Komal Jain <sprinklr@Komal-Jains-MacBook-Pro.local>
2017-02-10 13:41:37 +0530

    added filters, removed array

--- todo.js
+++ todo.js
@@ -1,23 +1,26 @@
 
 
-todoItems = {};
+// Holds the count of the tasks in the list
 
-const todoItemsLength = () => Object.keys(todoItems).length;
+let totalItems = 0;
+
+
+
 
 let addTodo = () => {
+
   let item = document.getElementById('todo-input').value;
 
   if(item=="") return;
 
-  if (todoItemsLength() == 0)
+  if (totalItems == 0)
     addFirstItem();
 
-
-  todoItems[todoItemsLength()] = item;
+  totalItems++;
 
   let list = document.getElementsByClassName('list')[0];
+
   let newItem = document.createElement('li');
-  newItem.id = "item"+ (todoItemsLength() - 1);
   newItem.className = 'active';
 
   let completeButton = document.createElement('input');
@@ -25,14 +28,14 @@ let addTodo = () => {
   completeButton.addEventListener('click', updateTodoStatus);
   newItem.appendChild(completeButton);
 
-  let itemText = document.createElement('span');
+  let itemText = document.createElement('div');
   itemText.className = 'todo-text';
   itemText.innerHTML = item;
   newItem.appendChild(itemText);
 
-  let deleteButton = document.createElement('span');
+  let deleteButton = document.createElement('div');
   deleteButton.className = 'delete';
-  deleteButton.innerHTML = '&#10005;';
+  deleteButton.innerHTML = '&#10005;';  //cross sign
   deleteButton.addEventListener('click', deleteTodo);
   newItem.appendChild(deleteButton);
 
@@ -42,6 +45,7 @@ let addTodo = () => {
 
 
 let updateTodoStatus = () => {
+
     let item = event.target.parentNode;
 
     if(event.target.checked) {
@@ -54,30 +58,133 @@ let updateTodoStatus = () => {
 
 
 let deleteTodo = () => {
+
     let item = event.target.parentNode;
-    let itemID = item.id[item.id.length-1];
-    delete todoItems[itemID];
+    item.parentNode.removeChild(item);
 
-    let parent = item.parentNode;
-    parent.removeChild(item);
+    totalItems--;
 
-    if(todoItemsLength() == 0)
-      removeLastItem();
+    if(totalItems == 0)
+       removeLastItem();
 
 }
 
+
+
+/* Add the ul element which will hold the list of tasks,
+ * and the footer containing filters
+ */
+
 let addFirstItem = () => {
+
   let listbody = document.getElementsByClassName('listbody')[0];
   let list = document.createElement('ul');
   list.className = "list";
   listbody.appendChild(list);
 
-  //add footer
+
   let footer = document.createElement('div');
+  footer.id = 'footer';
+
+  let all = document.createElement('span');
+  all.id = 'all-button';
+  all.addEventListener('click', updateList);
+  all.innerHTML = 'All';
+  all.className = 'selected';   //All filter is selected by default
+  footer.appendChild(all);
+
+  let active = document.createElement('span');
+  active.id = 'active-button';
+  active.addEventListener('click', updateList);
+  active.innerHTML = 'Active';
+  footer.appendChild(active);
+
+  let completed = document.createElement('span');
+  completed.id = 'completed-button';
+  completed.addEventListener('click', updateList);
+  completed.innerHTML = 'Completed';
+  footer.appendChild(completed);
+
+  listbody.appendChild(footer);
 
 }
 
+
+
+// Remove the ul element, and the footer
+
 let removeLastItem = () => {
+
   let list = document.getElementsByClassName('list')[0];
   list.parentNode.removeChild(list);
+
+  let footer = document.getElementById('footer');
+  footer.parentNode.removeChild(footer);
+}
+
+
+
+// Update the list according to filter selected
+
+let updateList = () => {
+
+  let selectedButton = event.target;
+
+  let allButton = document.getElementById('all-button');
+  let activeButton = document.getElementById('active-button');
+  let completedButton = document.getElementById('completed-button');
+
+  if(isSelected(selectedButton))
+    return;
+
+  selectedButton.classList.toggle('selected');
+
+
+  if(selectedButton == allButton) {
+
+    if(isSelected(activeButton)) {
+      activeButton.classList.toggle('selected');
+      changeDisplay('completed', '');
+    }
+    else {
+      completedButton.classList.toggle('selected');
+      changeDisplay('active', '');
+    }
+  }
+
+  else if(selectedButton == activeButton) {
+
+    changeDisplay('completed', 'none');
+
+    if(isSelected(allButton)) {
+      allButton.classList.toggle('selected');
+    }
+    else {
+      completedButton.classList.toggle('selected');
+      changeDisplay('active', 'none');
+    }
+  }
+
+  else if(selectedButton == completedButton) {
+
+    changeDisplay('active', 'none');
+
+    if(isSelected(activeButton)) {
+      activeButton.classList.toggle('selected');
+      changeDisplay('completed', '');
+    }
+    else {
+      allButton.classList.toggle('selected');
+    }
+  }
+
+}
+
+let isSelected = (element, cls) => element.className.indexOf('selected') > -1;
+
+let changeDisplay = (cls, prop) => {
+  let obj = document.getElementsByClassName(cls);
+  for(let i=0; i<obj.length; i++) {
+    obj[i].style.display = prop;
+  }
 }
